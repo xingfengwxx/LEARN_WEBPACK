dfb1a4fcf9fe8ac23ccda6abe385b51a3f115ade
WangXingxing <1099420259@qq.com>
2022-09-22 12:02:02 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# 学习Webpack
+## webpack是一个静态的模块化打包工具，为现代的JavaScript应用程序
+![图 1](images/README/IMG_20220922-111448025.png)
+
+[webpack官网]: https://webpack.js.org/
+
---
+++ src/vue/App.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Author: 王星星
+ * @Email: 1099420259@qq.com
+ * @Date: 2022-09-22 10:48:01
+ * @LastEditors: 王星星
+ * @LastEditTime: 2022-09-22 10:52:37
+ * @FilePath: \LEARN_WEBPACK\src\vue\App.vue
+ * @Description: 
+-->
+<template>
+  <h2>我是Vue渲染出来的</h2>
+  <h2>{{title}}</h2>
+  <hello-world></hello-world>
+</template>
+
+<script>
+  import HelloWorld from './HelloWorld';
+
+  export default {
+    components: {
+      HelloWorld
+    },
+    data() {
+      return {
+        title: "Hello World, wxx ",
+        message: "hehehe"
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+  h2 {
+    color: red;
+  }
+</style>
